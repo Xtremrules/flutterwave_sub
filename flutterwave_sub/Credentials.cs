71a91362bab94fb095163c41d5652b75787d8c98
Xtremrules <oleleweprince@outlook.com>
2018-07-30 16:45:02 +0100

    Added credential class

---
+++ flutterwave_sub/Credentials.cs
@@ -0,0 +1,8 @@
+﻿namespace flutterwave_sub
+{
+    public static class Credentials
+    {
+        public const string API_Secret_Key = "My Flutterwave API Secret Key";
+        public const string API_Public_Key = "My Flutterwave API Public Key";
+    }
+}
