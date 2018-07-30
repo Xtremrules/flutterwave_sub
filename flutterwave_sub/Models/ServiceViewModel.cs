b7618c8c78ad13ef33c0dd76163a00314a5baf63
Xtremrules <oleleweprince@outlook.com>
2018-07-30 12:47:10 +0100

    added service model class

---
+++ flutterwave_sub/Models/ServiceViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace flutterwave_sub.Models
+{
+    public class ServiceViewModel
+    {
+        public int Id { get; set; }
+        public int PlanId { get; set; }
+        public string name { get; set; }
+        public bool active { get; set; }
+    }
+}
