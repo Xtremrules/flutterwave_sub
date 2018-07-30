4fa9fe4986cdac49aa5f0a17ccfcfdb37e34ff8b
Xtremrules <oleleweprince@outlook.com>
2018-07-30 10:22:35 +0100

    Added simple domain classes

---
+++ flutterwave_sub/Models/MainDomain.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace flutterwave_sub.Models
+{
+    public class Service
+    {
+        public int id { get; set; }
+        [Required]
+        public int plan_id { get; set; }
+        [Required]
+        public string name { get; set; }
+        public virtual ICollection<Sub> Subs { get; set; }
+        public virtual ICollection<Manager> Managers { get; set; }
+    }
+
+    public class Manager
+    {
+        public int id { get; set; }
+        [Required]
+        public string account_name { get; set; }
+
+        public string ApplicationUserid { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        public virtual ICollection<Sub> Subs { get; set; }
+        public virtual ICollection<Service> Services { get; set; }
+    }
+
+    public class Sub
+    {
+        public int id { get; set; }
+        public string token { get; set; }
+
+        public string ApplicationUserid { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        public int Managerid { get; set; }
+        public virtual Manager Manager { get; set; }
+
+        public virtual ICollection<Service> Services { get; set; }
+    }
+}
