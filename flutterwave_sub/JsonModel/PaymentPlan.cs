e00da8b462f2673c35debad86cfd3b9522a08b17
Xtremrules <oleleweprince@outlook.com>
2018-07-30 23:29:42 +0100

    Added Json Payment plan

---
+++ flutterwave_sub/JsonModel/PaymentPlan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace flutterwave_sub.JsonModel
+{
+    public class PaymentPlan
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string amount { get; set; }
+        public string interval { get; set; }
+        public int duration { get; set; }
+        public string status { get; set; }
+        public string currency { get; set; }
+        public string plan_token { get; set; }
+        public DateTime date_created { get; set; }
+    }
+
+    public class PaymentPlanObject
+    {
+        public string status { get; set; }
+        public string message { get; set; }
+        public PaymentPlan data { get; set; }
+    }
+}
