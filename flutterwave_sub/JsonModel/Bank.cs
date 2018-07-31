99817f1ee73a84b3bce1c271ef8e5ca902438c60
Xtremrules <oleleweprince@outlook.com>
2018-07-31 11:09:37 +0100

    Added Json Bank response class

---
+++ flutterwave_sub/JsonModel/Bank.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace flutterwave_sub.JsonModel
+{
+    public class Bank
+    {
+        public string name { get; set; }
+        public string code { get; set; }
+        public string country { get; set; }
+    }
+
+    public class BankObject
+    {
+        public string status { get; set; }
+        public string message { get; set; }
+        public List<Bank> data { get; set; }
+    }
+}
