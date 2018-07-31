fd457bfdcc3dd3bdd9c66444eca0d7b57c56bdcb
Xtremrules <oleleweprince@outlook.com>
2018-07-31 14:36:23 +0100

    added card details encryption class

---
+++ flutterwave_sub/Encryption/IPaymentDataEncryption.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace flutterwave_sub.Encryption
+{
+    public interface IPaymentDataEncryption
+    {
+        string GetEncryptionKey(string secretKey);
+        string EncryptData(string encryptionKey, String data);
+        string DecryptData(string encryptedData, string encryptionKey);
+    }
+}
