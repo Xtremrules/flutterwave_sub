237fc704ad791fec8739d705096a801bd03a84c1
Xtremrules <olelewextremrules@gmail.com>
2018-08-03 01:13:38 +0100

    Update and rename 3.sql to roles.sql
    Run this to create default roles

---
+++ sql/roles.sql
@@ -0,0 +1,8 @@
+USE [aspnet-flutterwave_sub-20180730100014]
+GO
+INSERT [dbo].[Roles] ([RoleId], [Name]) VALUES (N'fba3b34b-7b22-4428-af5b-522834b84381', N'admin')
+GO
+INSERT [dbo].[Roles] ([RoleId], [Name]) VALUES (N'2b8c0e6f-54db-42cf-9bc3-71f585bfaf9d', N'manager')
+GO
+INSERT [dbo].[Roles] ([RoleId], [Name]) VALUES (N'300c5c09-e46b-43b8-8f8e-a328887f3f8a', N'tenat')
+GO
